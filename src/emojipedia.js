ea1b836ba8d164c726b7e819e0f8501390096bc8
Luka <lukabasicjr@gmail.com>
2021-12-02 16:12:00 +0100

    thirdcommit

---
+++ src/emojipedia.js
@@ -0,0 +1,33 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "📈",
+    name: "Promet za današnji dan",
+    meaning:
+      "11.573,50 kn"
+  },
+  {
+    id: 2,
+    emoji: "🏦",
+    name: "Ukupni promet za 3. Mjesec",
+    meaning:
+      "735.465,00 kn"
+  },
+  {
+    id: 3,
+    emoji: "🥇",
+    name: "5 najboljih aparata",
+    meaning:
+      "Esplanada, Antunović, Milletech, NK Drvo, Peti"
+  },
+  {
+    id: 4,
+    emoji: "☕",
+    name: "5 najprodavanijih proizvoda",
+    meaning:
+      "Kava, Cappuccino, Topla čokolada, Kit-kat, Snickers"
+  }
+
+];
+
+export default emojipedia;
